23262ba53d604f3844c8916c5543361e59c84f63
Pawel Dyjak <pawel.dyjak2@gmail.com>
2017-10-02 08:43:00 +0200

    Events - ability to add weak listeners

--- wear/src/main/java/com/pdyjak/powerampwear/common/Events.kt
+++ wear/src/main/java/com/pdyjak/powerampwear/common/Events.kt
@@ -1,9 +1,23 @@
 package com.pdyjak.powerampwear.common
 
+import java.util.*
+import kotlin.collections.HashSet
+
 abstract class EventArgs
 
+// TODO: remove duplication
+
 class SimpleEvent {
-    private val mEventHandlers: HashSet<() -> Unit> = HashSet()
+    private val mEventHandlers: MutableSet<() -> Unit>
+    private val mWeak: Boolean
+    private var mWeakEvent: SimpleEvent? = null
+
+    constructor() : this(HashSet(), false)
+
+    private constructor(handlersContainer: MutableSet<() -> Unit>, weak: Boolean) {
+        mEventHandlers = handlersContainer
+        mWeak = weak
+    }
 
     operator fun plusAssign(eventHandler: () -> Unit) {
         mEventHandlers.add(eventHandler)
@@ -16,11 +30,29 @@ class SimpleEvent {
     fun notifyEventChanged() {
         val copy = HashSet(mEventHandlers)
         for (handler in copy) handler.invoke()
+        if (mWeakEvent != null) mWeakEvent!!.notifyEventChanged()
+    }
+
+    fun weakly(): SimpleEvent {
+        if (mWeak) throw IllegalStateException("Already weak!")
+        if (mWeakEvent != null) return mWeakEvent!!
+        mWeakEvent = SimpleEvent(Collections.newSetFromMap(
+                WeakHashMap<() -> Unit, Boolean>()), true)
+        return mWeakEvent!!
     }
 }
 
 class Event<ArgsType : EventArgs?> {
-    private val mEventHandlers: HashSet<(ArgsType) -> Unit> = HashSet()
+    private val mEventHandlers: MutableSet<(ArgsType) -> Unit>
+    private val mWeak: Boolean
+    private var mWeakEvent: Event<ArgsType>? = null
+
+    constructor() : this(HashSet(), false)
+
+    private constructor(handlersContainer: MutableSet<(ArgsType) -> Unit>, weak: Boolean) {
+        mEventHandlers = handlersContainer
+        mWeak = weak
+    }
 
     operator fun plusAssign(eventHandler: (ArgsType) -> Unit) {
         mEventHandlers.add(eventHandler)
@@ -33,5 +65,14 @@ class Event<ArgsType : EventArgs?> {
     fun notifyEventChanged(args: ArgsType) {
         val copy = HashSet(mEventHandlers)
         for (handler in copy) handler.invoke(args)
+        if (mWeakEvent != null) mWeakEvent!!.notifyEventChanged(args)
+    }
+
+    fun weakly(): Event<ArgsType> {
+        if (mWeak) throw IllegalStateException("Already weak!")
+        if (mWeakEvent != null) return mWeakEvent!!
+        mWeakEvent = Event(Collections.newSetFromMap(
+                WeakHashMap<(ArgsType) -> Unit, Boolean>()), true)
+        return mWeakEvent!!
     }
 }
